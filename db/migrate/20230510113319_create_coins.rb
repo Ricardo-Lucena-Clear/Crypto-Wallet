fed681c311ecdaf86e2ded7058740cf622852331
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-10 08:36:00 -0300

    Creating the first CRUD using Scaffold

---
+++ db/migrate/20230510113319_create_coins.rb
@@ -0,0 +1,12 @@
+class CreateCoins < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coins do |t|
+      t.string :description
+      t.string :acronym
+      t.string :string
+      t.string :url_image
+
+      t.timestamps
+    end
+  end
+end
