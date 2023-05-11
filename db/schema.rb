5bd890147382c2a0c7ab8aa46cad78b1b61a4865
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-11 12:48:07 -0300

    Creating the 2nd CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_10_113319) do
+ActiveRecord::Schema.define(version: 2023_05_11_150916) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
     t.string "acronym"
     t.string "url_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "mining_types", force: :cascade do |t|
+    t.string "name"
+    t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
