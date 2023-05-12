b8393a3d78ff4ff8282899ab8173821bffc608d3
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-12 12:19:09 -0300

    Precompiling and isolating assets by controller

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,5 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( coins.css welcome.css scaffolds.css mining_types.css )
+Rails.application.config.assets.precompile += %w( coins.js welcome.js scaffolds.js mining_types.js cable.js )
