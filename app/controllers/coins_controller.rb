9f32832b0036e79f734ee97450c32c1d1db864c2
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-12 09:24:45 -0300

    Defaulting select to MVC

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,6 +1,7 @@
 class CoinsController < ApplicationController
   layout "adm"
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :edit, :update, :create]
 
   # GET /coins or /coins.json
   def index
@@ -59,6 +60,12 @@ class CoinsController < ApplicationController
   end
 
   private
+
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+      
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
