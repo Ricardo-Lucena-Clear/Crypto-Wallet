541cccf971ea1d2420ecc238d21ed9e540383d19
Ricardo-Lucena-Clear <ricardo.lucena.clear@gmail.com>
2023-05-12 10:10:34 -0300

    Activating the i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,20 @@
 module ApplicationHelper
-    def data_br(data_us)
-        data_us.strftime("%d/%m/%Y")
-        
-    end
-    def ambiente_rails
-        if Rails.env.development?
-            "Desenvolvimento"
-        elsif Rails.env.production?
-            "Produção"
-        else 
-            "Teste"
-        end
-        
-    end
+  def locale(locale)
+    I18n.locale == :en ? "Estados Unidos" : "Português do Brasil"
+  end
 end
+
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
+
